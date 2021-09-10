807f477d0fae5eb39487d8f384f693802be33ce8
oil <>
2021-09-10 17:59:40 +0700

    Add comment

--- Lesson2/ifelse.py
+++ Lesson2/ifelse.py
@@ -33,4 +33,14 @@ if __name__ == '__main__':
     #If n is even and greater than 20 , print Not Weird
     if (n % 2 == 0) & (n > 20):
         print ("Not Weird")
-        
+
+"""
+    if n % 2 == 1:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")    
+"""    
