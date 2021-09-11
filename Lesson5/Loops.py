837d5d0d4f24a6908536c696d4cc16bbe2cccdd8
oil <>
2021-09-11 15:48:16 +0700

    Add Loops

---
+++ Lesson5/Loops.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    i = 0
+    while i < n:
+        #j = i * i
+        #print (j)
+        print (i * i)
+        i = i + 1 #i += 1
+
+"""
+for i in range(0, 5):
+    print (i)
+ 
+for i in range(0, n):
+        j = i * i
+        print (j)
+"""
+
+#i = 0
+#while i < 5:
+    #print (i)
+    #i += 1
+    #i = i+1
