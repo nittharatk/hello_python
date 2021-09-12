aa6a2f2e263ec4612ca233ce418e5f26a28fd47f
oil <>
2021-09-12 14:15:20 +0700

    Add Printfunction

---
+++ Lesson6/PrintFunction.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+
+    '''  
+    i = 1
+    while i <= n:
+        print (i, end='')
+        i += 1
+    '''  
+    
+    for i in range(1,n+1):
+        print(i, end='')
