41becd3563506ec2663d20c8bc9a0d6e3128eecf
oil <>
2021-09-13 22:00:26 +0700

    Add Write a function

--- Lesson7/writeafuction.py
+++ Lesson7/writeafuction.py
@@ -38,3 +38,18 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
+
+"""
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if (year % 4 == 0):
+        leap = True
+        if (year % 100 == 0):
+            leap = False
+            if (year % 400 == 0):
+                leap = True
+  
+    return leap
+"""
